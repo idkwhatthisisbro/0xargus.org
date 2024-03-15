3239559b6e2a273595f53ba782f0c3fa2f269c46
0xargusorg <162007694+0xargusorg@users.noreply.github.com>
2024-03-15 12:17:14 +0000

    Delete src/lib/supabaseClient.ts

--- src/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-	'https://rzghccacifenrcnbinyn.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6Z2hjY2FjaWZlbnJjbmJpbnluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk0NTE2ODUsImV4cCI6MjAyNTAyNzY4NX0.0NptDsICXCxqRS2Tw8v3mmUXwWYiG3XncbIbLZrbhFQ'
-);
