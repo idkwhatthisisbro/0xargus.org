b7791478de6180b78d060c62ce84b6af70b901b4
0xargusorg <162007694+0xargusorg@users.noreply.github.com>
2024-03-15 12:17:58 +0000

    Add files via upload

---
+++ src/lib/supabase/supabase.ts
@@ -0,0 +1,23 @@
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './types';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+
+export const supabase = createClient<Database>(
+	PUBLIC_SUPABASE_URL,
+	PUBLIC_SUPABASE_ANON_KEY
+);
+
+
+export const signUp = async (email: string) => {
+	const password = 'teCghv7xWMryQMAxCxfC'
+	const user = await supabase.auth.signUp({email, password})
+
+	return user
+}
+
+// export const getUser = async (email: string) => {
+// 	const password = 'teCghv7xWMryQMAxCxfC'
+// 	const user = await supabase.auth.getUser(email, password)
+
+// 	return user
+// }
