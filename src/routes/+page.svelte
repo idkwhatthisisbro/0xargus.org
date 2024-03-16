41338f27e5f56b857bbca1d15433c6512a858d1d
Argus <team@0xargus.org>
2024-03-16 15:13:18 +0000

    Update hero text animation to include overflow-hidden class.
    - Added overflow-hidden class to the div element for better text display in the hero section.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -287,7 +287,7 @@
 			<!-- HERO TEXT -->
 			<div style="--cursor-width: 0.125ch; --cursor-color: #ffffff;" class="relative mb-24 text-center font-outfit text-4xl text-white sm:mt-8">
 				Introducing RugPull Prevention Middleware for
-				<div class="hidden">
+				<div class="overflow-hidden">
 					<TypeWriter element="span" class="absolute inset-0 whitespace-nowrap" cursor={false} as="span" mode="loop" wordInterval={4000}>
 						<span class="inline-block font-semibold text-purple-500">Ethereum.</span>
 						<span class="inline-block font-semibold text-indigo-500">Solana.</span>
