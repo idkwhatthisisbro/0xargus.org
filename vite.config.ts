9d618f8833c874d04ab703a49af9c1e21a184d76
Argus <team@0xargus.org>
2024-04-15 20:02:18 +0100

    Added console removal plugin to Vite config
    The Vite configuration has been updated with a new plugin that removes console logs in production. This change helps to keep the production code clean and free of unnecessary logging statements.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
+import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		// Removes console logs in production
+		removeConsole(),
 		svg({
 			svgoOptions: {
 				plugins: [
