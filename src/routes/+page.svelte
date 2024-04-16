9aedb51d936a2f3d9d8f612c6a95ec9cfe0cd166
0xArgus <admin@0xargus.org>
2024-04-16 09:01:51 +0700

    Updated UI components and icons
    - Modified the footer slot in JoinTheWhitelist component for better specificity
    - Adjusted variable declaration order in AccordionTrigger component
    - Removed Spotlight component from the project as it's no longer needed
    - Updated icon imports and usage in +page.svelte, replaced SVGs with lucide-svelte icons for consistency and cleaner code
    - Added 'footer' slot to Footer component within JoinTheWhitelist
    "Revamped presale page and updated dependencies
    
    - Removed optional peerDependenciesMeta from pnpm-lock.yaml
    - Deleted unused .svelte file in src/routes
    - Overhauled the layout, content, and styling of the presale page for better user experience
    - Replaced certain icons with new ones for visual consistency
    - Added two new static files: cardanoCoin.png and tg-welcomeBanner.gif"
    
    Refactored layout and improved accessibility
    
    - Simplified the structure of elements in +page.svelte
    - Removed redundant code blocks for cleaner, more maintainable code
    - Enhanced accessibility by adding roles and aria-labels to interactive elements
    - Adjusted responsive design for better user experience across different device sizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -385,21 +385,8 @@
 
 						<EvervaultCard text={circle} />
 
-							<h2 class="mt-4 text-base font-light text-white">{desc}</h2>
-							<p class="mt-4 rounded-full border border-white/[0.2] px-2 py-0.5 text-sm font-light text-white">{tag}</p>
-						</div>
-					{/each}
-					<!-- M DESKTOP SUBROW -->
-					<div class="group hidden md:block lg:hidden">
-						<GlowingStarsBackgroundCard>
-							<GlowingStarsTitle>Invest Fearlessly</GlowingStarsTitle>
-							<div class="flex items-end justify-between">
-								<GlowingStarsDescription>Feel safe investing in small coins. Seamlessly integrate rugpull protection into all your favorite exchanges.</GlowingStarsDescription>
-								<div class="flex h-8 w-8 items-center justify-center rounded-full border-2 border-red-500 bg-neutral-800 p-2 duration-200 ease-in-out group-hover:bg-neutral-900">
-									<MoveRightIcon class="h-4 w-4 stroke-2 text-white" />
-								</div>
-							</div>
-						</GlowingStarsBackgroundCard>
+						<h2 class="mt-4 text-base font-light text-white">{desc}</h2>
+						<p class="mt-4 rounded-full border border-white/[0.2] px-2 py-0.5 text-sm font-light text-white">{tag}</p>
 					</div>
 				{/each}
 				<!-- M DESKTOP SUBROW -->
@@ -408,22 +395,36 @@
 						<GlowingStarsTitle>Invest Fearlessly</GlowingStarsTitle>
 						<div class="flex items-end justify-between">
 							<GlowingStarsDescription>Feel safe investing in small coins. Seamlessly integrate rugpull protection into all your favorite exchanges.</GlowingStarsDescription>
-							<div class="flex h-8 w-8 items-center justify-center rounded-full bg-neutral-800 p-2 duration-200 ease-in-out group-hover:bg-neutral-900">
-								<a
-									role="button"
-									tabindex="0"
-									aria-label="GLOWING to documentation"
-									on:click={() => window.open('https://docs.0xargus.org/presale', '_blank')}
-									on:keydown={(event) => {
-										if (event.key === 'Enter') window.open('https://0xargus.org/presale', '_blank');
-									}}
-									href="https://docs.0xargus.org"
-									target="_blank"
-									class="flex h-8 w-8 items-center justify-center rounded-full bg-neutral-800 p-2 duration-200 ease-in-out group-hover:bg-neutral-900">
-									<MoveRightIcon class="h-4 w-4 stroke-2 text-white transition duration-200 group-hover:translate-x-0.5" />
-								</a>
-								<MoveRightIcon class="h-4 w-4 stroke-2 text-white transition duration-200 group-hover:translate-x-0.5" />
+							<div class="flex h-8 w-8 items-center justify-center rounded-full border-2 border-red-500 bg-neutral-800 p-2 duration-200 ease-in-out group-hover:bg-neutral-900">
+								<MoveRightIcon class="h-4 w-4 stroke-2 text-white" />
 							</div>
+						</div>
+					</GlowingStarsBackgroundCard>
+				</div>
+			</div>
+
+			<!-- GLOWING STARS CARD -->
+			<!-- MOBILE, TABLET AND LARGE DEVICES -->
+			<a href="https://docs.0xargus.org" class="group block min-h-96 cursor-pointer md:hidden lg:block">
+				<GlowingStarsBackgroundCard>
+					<GlowingStarsTitle>Invest Fearlessly</GlowingStarsTitle>
+					<div class="flex items-end justify-between">
+						<GlowingStarsDescription>Feel safe investing in small coins. Seamlessly integrate rugpull protection into all your favorite exchanges.</GlowingStarsDescription>
+						<div class="flex h-8 w-8 items-center justify-center rounded-full bg-neutral-800 p-2 duration-200 ease-in-out group-hover:bg-neutral-900">
+							<a
+								role="button"
+								tabindex="0"
+								aria-label="GLOWING to documentation"
+								on:click={() => window.open(BASE_URL + '/presale', '_blank')}
+								on:keydown={(event) => {
+									if (event.key === 'Enter') window.open(BASE_URL + '/presale', '_blank');
+								}}
+								href="https://docs.0xargus.org"
+								target="_blank"
+								class="flex h-8 w-8 items-center justify-center rounded-full bg-neutral-800 p-2 duration-200 ease-in-out group-hover:bg-neutral-900">
+								<MoveRightIcon class="h-4 w-4 stroke-2 text-white transition duration-200 group-hover:translate-x-0.5" />
+							</a>
+							<MoveRightIcon class="h-4 w-4 stroke-2 text-white transition duration-200 group-hover:translate-x-0.5" />
 						</div>
 					</div>
 				</GlowingStarsBackgroundCard>
--- src/routes/.svelte
+++
@@ -1,3 +0,0 @@
-<div>
-    Presale Launches on
-</div>
