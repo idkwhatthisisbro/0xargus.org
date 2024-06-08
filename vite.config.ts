f6a793c0a449cb53385d87ea3cb009aba0e3db95
idkwhatthisisbro <admin@0xargus.org>
2024-06-08 07:01:19 +0700

    Updated Vite configuration
    Removed the 'partytownVite' plugin from the Vite configuration. This simplifies the build process by eliminating unnecessary worker thread operations.

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import svg from '@poppanator/sveltekit-svg';
 import removeConsole from 'vite-plugin-remove-console';
-import { partytownVite } from '@builder.io/partytown/utils';
 import path from 'path';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
-		// Runs stuff in worker thread
-		partytownVite({
-			dest: path.join(__dirname, 'dist', '~partytown')
-		}),
-
 		// Removes console logs in production
 		removeConsole(),
 		sveltekit(),
